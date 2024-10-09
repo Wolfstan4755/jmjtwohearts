1e3fdfc6cb68db517ac9c76adbe534d291b1578a
Salvador Tapia <salvadotapia324@gmail.com>
2024-10-08 22:40:10 -0400

    add a lot of changes

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+// Example: Add a console log
+console.log("JavaScript is running!");
+
+// Example: Change the text of an h1 element
+document.addEventListener('DOMContentLoaded', function() {
+    const heading = document.querySelector('h1');
+    heading.textContent = "This text was changed by JavaScript!";
+});
